976634f2441bbf3c95aa9c3f7367d9261be4927c
RhysCruz <rhyscruz16@gmail.com>
2019-02-22 15:13:02 +0000

    Actor Model First Draft

---
+++ MovieWebSite/MovieWebSite/Models/Actor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieWebSite.Models
+{
+    public class Actor
+    {
+        public virtual int ActorID { get; set; }
+        public virtual string ActorName { get; set; }
+        public virtual string ActorBio { get; set; }
+        public virtual DateTime ActorDOB { get; set; }
+        public virtual string ActorRoles { get; set; }
+    }
+}
