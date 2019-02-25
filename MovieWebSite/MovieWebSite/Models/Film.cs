54bf709a3b504bebe3fdc907ed5eac0fc93efc1a
RhysCruz <rhyscruz16@gmail.com>
2019-02-25 12:59:11 +0000

    FilmModel1st

---
+++ MovieWebSite/MovieWebSite/Models/Film.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieWebSite.Models
+{
+    public class Film
+    {
+        public virtual int FilmID { get; set; }
+        public virtual string FilmName { get; set; }
+        public virtual string FilmDescription { get; set; }
+        public virtual DateTime ReleaseDate { get; set; }
+    }
+}
